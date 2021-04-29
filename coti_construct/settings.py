618f18e359fdf5c54d07347dbef97ed7b2f465c1
Carlos Hdez <carlos@puntoreica.com>
2021-04-29 14:00:40 -0500

    auth token

--- coti_construct/settings.py
+++ coti_construct/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'coti',
+    'users',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
--- coti_construct/urls.py
+++ coti_construct/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from users.views import CustomAuthToken
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-token-auth/', CustomAuthToken.as_view()),
 ]
